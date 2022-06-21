786630f0caa18babd1cb6073570f13daf03532b6
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-21 13:24:15 -0300

    Initial Commit

---
+++ rxswift-weatherUITests/rxswift_weatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rxswift_weatherUITestsLaunchTests.swift
+//  rxswift-weatherUITests
+//
+//  Created by Raphael Torquato on 21/06/22.
+//
+
+import XCTest
+
+class rxswift_weatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
